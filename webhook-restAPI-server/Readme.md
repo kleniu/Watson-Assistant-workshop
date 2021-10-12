34ac1e8dafe10c595d48fde9fa29e2e94a990daf
Robert Kleniewski <robert.kleniewski@pl.ibm.com>
2021-10-12 11:56:24 +0200

    making REST API for WA

--- webhook-restAPI-server/Readme.md
+++ webhook-restAPI-server/Readme.md
@@ -1,8 +1,19 @@
 ## Przykłądy wywołań
 Kika komend (uwielbiam CLI i REST) :)
+
+Zakładam, ze zgodnie z konfiguracją w manifest.yml deployment zrobiłeś do CLoudFoudry w IBM Cloud pod nazwą "webhook-restapi-server". Jezeli się nie mylę to funkcje REST API dostępne są pod URI `https://webhook-restapi-server.eu-de.mybluemix.net/api`. 
 ```
-curl -X POST -H 'Content-Type: application/json' -d '{"id":"JSI243"}' http://localhost:8080/getbyid
-curl -X POST -H 'Content-Type: application/json' -d '{"email":"robert@test.pl", "date":"21/09/2021"}' http://localhost:8080/getbydateandemail
+## pobierz wszystkie zlecenia
+curl -X POST -H 'Content-Type: application/json' -d '{"action":"getall"}' http://webhook-restapi-server.eu-de.mybluemix.net/api | jq
+
+## pobierz pojedyncze zlecenie idendyfikowane przez klucz: email:data
+curl -X POST -H 'Content-Type: application/json' -d '{"action":"getbydateandemail", "email":"rafal@agro.eu", "date":"2021-10-11"}' http://webhook-restapi-server.eu-de.mybluemix.net/api | jq
+
+## zmień status zlecenia identyfikowanego przez klucz: email:data na "anulowano"
+curl -X POST -H 'Content-Type: application/json' -d '{"action":"abortdelivery", "email":"rafal@agro.eu", "date":"2021-10-11"}' http://webhook-restapi-server.eu-de.mybluemix.net/api | jq
+
+## dane o zleceniach trzymane są w pamięci :) przywróć dane oryginalne
+curl -X POST -H 'Content-Type: application/json' -d '{"action":"restart"}' http://webhook-restapi-server.eu-de.mybluemix.net/api | jq
 ```
 
 ## Przepis na uruchomienie serwera REST w Internecie
--- webhook-restAPI-server/app.js
+++ webhook-restAPI-server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-// fake data 
-var datarepo = require('./data/data.js');
+
+// fake datatabase
+const initialdata = [
+  { id: 'ZAM243', email: 'robert@test.pl', status: 'sent', date: '2021-09-12', time: '12:01:00'},
+  { id: 'ZAM731', email: 'rafal@agro.eu', status: 'completing', date: '2021-10-11', time: '16:12:01'},
+];
+let datarepo = JSON.parse(JSON.stringify(initialdata));
 
 // we will be working with JSON
 app.use(express.json());
@@ -13,30 +18,40 @@ app.get('/', (rer, res) => {
   res.send('Żyję i mam się dobrze :)');
 });
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get('/api/ping', (req, res) => {
+  res.send( { response:"pong" } );
 });
 
-app.post('/getall', (req, res) => {
-  res.send(JSON.stringify(datarepo.data));
-});
-
-app.post('/getbyid', (req, res) => {
-  console.log('DEBUG req.body =', req.body);
-  var retval = datarepo.data.find( element => element.id === req.body.id );
-  if ( typeof retval === 'undefined') retval = '{}';
-  res.send(JSON.stringify(retval));
+app.post('/api', (req, res) => {
+  var retval = {};
+  switch(req.body.action) {
+    case 'getall':
+      retval = JSON.stringify(datarepo);
+      break;
+    case 'getbydateandemail':
+      retval = datarepo.find( 
+        element => element.date === req.body.date && element.email === req.body.email 
+      );
+      break;
+    case 'getbyid':
+      retval = datarepo.find( element => element.id === req.body.id );
+      break;
+    case 'abortdelivery':
+      var i = datarepo.findIndex(
+        element => element.date === req.body.date && element.email === req.body.email 
+      );
+      if ( i != -1 ) {
+        datarepo[i].status = 'alunowano';
+        retval = datarepo[i];
+      }
+      break;
+    case 'restart':
+      datarepo = JSON.parse(JSON.stringify(initialdata));
+      retval = { response:"Pracujesz teraz na danych inicjalnych. Powodzenia!" };
+      break;
+  }
+  res.send(retval);
 })
-
-app.post('/getbydateandemail', (req, res) => {
-  console.log('DEBUG req.body =', req.body);
-  var retval = datarepo.data.find( 
-    element => element.date === req.body.date && element.email === req.body.email 
-  );
-  if ( typeof retval === 'undefined') retval = '{}';
-  res.send(JSON.stringify(retval));
-})
-
 
 app.listen(port, () => {
   console.log(`Rest API app is listening at http://localhost:${port}`)
